851ee43b72376cbe13d5b74d06af2b0f54c7f2ff
Bo Johnson <bojohnson@Bos-MacBook-Pro.attlocal.net>
2025-02-03 21:26:35 -0500

    add return type to main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use rust_daq::felib_open;
+use rust_daq::*;
 
-fn main() {
+fn main() -> Result<(), FELibError> {
     println!("Hello, world!");
     let res = felib_open("dig2://caendgtz-usb-25380");
     match res {
         Ok(v) => println!("opened digitizer with handle: {}", v),
         Err(e) => println!("can't open digitizer: {:?}", e),
     }
+
+    Ok(())
 }
